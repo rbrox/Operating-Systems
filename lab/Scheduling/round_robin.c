bed973cfbc9c1a0f97ca4d9d4046b84c7efa821b
rbrox <rishavsci@gmail.com>
2023-08-06 11:42:14 +0530

    rr

--- lab/Scheduling/round_robin.c
+++ lab/Scheduling/round_robin.c
@@ -6,19 +6,33 @@
 #define MAX 10 // Max process count
 int process_count;
 int process_id[MAX];
-int priority[MAX];
 int burst_time[MAX];
+int remaining_burst_time[MAX];
 int wait_time[MAX];
 int turn_around_time[MAX];
 
 int order[100];
 int order_count = 0;
 
+int time_slice = 4;
+
 /*
 The things we take as input
 1. Number of Processes
 2. Burst Times
 */ 
+
+int swt(){
+    int sum = 0;
+    for (int i = 0; i < process_count; sum += remaining_burst_time[i], ++i);
+
+    if (sum == 0){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
 
 void input(){
     
@@ -29,60 +43,41 @@ void input(){
         printf("Too many processes");
         exit(0);
     }
+    printf("Enter Time-Slice: ");
+    scanf("%d", &time_slice);
     
+
     for(int i = 0; i < process_count; ++i){
         printf("\nEnter p[%d] burst time: ", i);
         scanf("%d", &burst_time[i]);
-        printf("\nEnter p[%d] prority: ", i);
-        scanf("%d", &priority[i]);
+        remaining_burst_time[i] = burst_time[i];
     }
 }
 
 // Perform Shortest Job First Scehduling
 void schedule(){
-    for(int i = 0; i < MAX; process_id[i] = i , ++i);
-    // Sorting the burst time array and filling process id so as to keep track of pid's
-    int min_index, min;
-    for (int i = 0; i < process_count; ++i){
-        min = priority[i];
-        min_index = i;
-        for (int j = i + 1; j < process_count; ++j){
-            if (min > priority[j]){
-                min_index = j;
-                min = priority[j];
-            }
-        }
-
-        // Swap
-        int temp = priority[i];
-        priority[i] = min;
-        priority[min_index] = temp;
-
-        // Maintain burst times
-        temp = burst_time[i];
-        burst_time[i] = burst_time[min_index];
-        burst_time[min_index] = temp;
-
-        // Maintain process_id 
-        temp = process_id[i];
-        process_id[i] = process_id[min_index];
-        process_id[min_index] = temp;
-    }
-    printf("\n\tSorted....\n");
-    for(int i = 0; i < process_count; ++i){
-        printf("\t%d %d %d\n", priority[i], process_id[i], burst_time[i]);
-    }
-
-    
+    for (int i = 0; i < process_count; wait_time[i] = 0, ++i);
     int curr_time = 0;
-    for (int i = 0; i < process_count; ++i){
-        wait_time[i] = curr_time;
+    int curr_job = 0;
+    while(1){
+        wait_time[curr_job] += curr_time;
 
-        curr_time += burst_time[i];
-        turn_around_time[i] = curr_time;
-
-        order[order_count++] = process_id[i];
+        if(remaining_burst_time[curr_job] > time_slice){
+            remaining_burst_time[curr_job] -= time_slice;
+            curr_time += time_slice;
+            order[order_count++] = curr_job;
+        }
+        else if (remaining_burst_time > 0){
+            curr_time += remaining_burst_time[curr_job];
+            remaining_burst_time[curr_job] = 0;
+            turn_around_time[curr_job] = curr_time;
+            order[order_count++] = curr_job;
+        }
+        if (swt){
+            break;
+        }
     }
+
 }
 
 void display()
